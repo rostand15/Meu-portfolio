b14428b2937896f28c5a40c7ec68a17a13acad8e
Rostand Araújo <rostandaraujo3@gmail.com>
2026-03-03 21:51:56 -0300

    Refactor email sending logic with input sanitization

--- enviar.php
+++ enviar.php
@@ -1,21 +1,32 @@
 <?php
 
-    $nome = addcslashes($_POST('nome'));
-    $email = addcslashes($_POST('email'));
-    $telefone = addcslashes($_POST('telefone'));
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_SPECIAL_CHARS);
+    $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_SPECIAL_CHARS);
 
     $to = "rstdpinheiro1@gmail.com";
-    $assunto = "Contato cliente";
+    $assunto = "Contato cliente - Site";
 
-    $corpo = "Nome: " .$nome. "\n"."E-mail: " .email. "\n". "Telefone: " .$telefone;
+    $corpo = "Nome: " . $nome . "\n" .
+             "E-mail: " . $email . "\n" .
+             "Telefone: " . $telefone . "\n\n" .
+             "Mensagem:\n" . $mensagem;
 
-    $cabeca = "From rostandaraujo3@gmail.com"."\n"."Reply-to: " .$email. "\n"."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: rostandaraujo3@gmail.com\r\n";
+    $cabeca .= "Reply-To: " . $email . "\r\n";
+    $cabeca .= "X-Mailer: PHP/" . phpversion();
 
-    if(mail($to, $assunto, $corpo, $cabeca)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o email!");
+    if (mail($to, $assunto, $corpo, $cabeca)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar o e-mail!";
     }
 
+} else {
+    echo "Acesso inválido.";
+}
 
 ?>
